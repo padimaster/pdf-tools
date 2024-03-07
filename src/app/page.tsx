d28046e4f036244fcc03ea7767013982b3dad4d0
Alex Padilla <0xpadimaster@gmail.com>
2024-03-07 11:03:05 -0500

    feat: api implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,44 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import PDFCard from "@/components/pdf/pdf-card.component";
-import Image from "next/image";
-import Link from "next/link";
-import { CgMergeVertical } from "react-icons/cg";
+'use client';
+import PDFCard from '@/components/pdf/pdf-card.component';
+import { BsFiletypeDocx } from 'react-icons/bs';
+import { FaCompressArrowsAlt, FaFileImage } from 'react-icons/fa';
+import { SlDocs } from 'react-icons/sl';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold mb-10">PDF Tools</h1>
-      <section className="w-full flex flex-wrap gap-4 justify-center">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center py-8 md:p-24">
+      <h1 className="text-3xl md:text-6xl font-bold mb-10">PDF Tools</h1>
+      <section className="max-w-[40rem] grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 justify-center content-center place-items-center">
         <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
+          title="Word to PDF"
+          description="Effortlessly convert Word documents into a unified PDF file."
+          icon={BsFiletypeDocx}
+          endpoint="/word-pdf"
+          color="blue"
         />
+
         <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
+          title="JPG to PDF"
+          description="Combine multiple JPG images into a consolidated PDF document."
+          icon={FaFileImage}
+          endpoint="/jpg-pdf"
+          color="green"
         />
+
         <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
+          title="PDF Merge Tool"
+          description="Integrate various PDF files into a single, organized document."
+          icon={SlDocs}
+          endpoint="/merge"
+          color="red"
         />
+
         <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
-        />
-        <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
-        />
-        <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
-        />
-        <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
-        />
-        <PDFCard
-          href="/pdf/merge"
-          title="Merge PDFs"
-          description="Merge multiple PDF files into a single PDF."
+          title="PDF Compression"
+          description="Efficiently compress PDF files to reduce their size without compromising quality."
+          icon={FaCompressArrowsAlt}
+          endpoint="/compress"
+          color="purple"
         />
       </section>
     </main>
--- src/components/pdf/pdf-card.component.tsx
+++ src/components/pdf/pdf-card.component.tsx
@@ -1,21 +1,165 @@
-import Link from "next/link";
-import React from "react";
-import { Card, CardContent, CardHeader } from "../ui/card";
-import { CgMergeVertical } from "react-icons/cg";
+'use client';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { config } from '@/config';
+import { cn } from '@/lib/utils';
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { IconType } from 'react-icons';
+import { FaRegFilePdf } from 'react-icons/fa';
+import { IoReload } from 'react-icons/io5';
+import { Button } from '../ui/button';
+import { Card, CardContent, CardHeader } from '../ui/card';
 
-export default function PDFCard({ title, description, icon, href }: any) {
-  const Icon = icon;
+const colors = {
+  red: { bg: 'bg-red-500/20', text: 'text-red-500' },
+  blue: { bg: 'bg-blue-500/20', text: 'text-blue-500' },
+  green: { bg: 'bg-green-500/20', text: 'text-green-500' },
+  purple: { bg: 'bg-purple-500/20', text: 'text-purple-500' },
+};
+
+export default function PDFCard({
+  title,
+  description,
+  icon: Icon,
+  color = 'blue',
+  endpoint,
+}: {
+  title: string;
+  description: string;
+  icon: IconType;
+  color?: keyof typeof colors;
+  endpoint: string;
+}) {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = new FileReader();
+
+    file.readAsArrayBuffer(acceptedFiles[0]);
+  }, []);
+
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
+    useDropzone({
+      onDrop,
+      accept: {
+        'application/pdf': ['.pdf'],
+        'image/jpeg': ['.jpg', '.jpeg'],
+        'application/msword': ['.doc'],
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+          ['.docx'],
+      },
+    });
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    setLoading(true);
+    if (!acceptedFiles) return;
+
+    const formData = new FormData();
+
+    acceptedFiles.forEach((file) => {
+      formData.append(`files`, file);
+    });
+
+    const result = await fetch(`${config.api.url}${endpoint}`, {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await result.blob();
+    setLoading(false);
+
+    const blob = new Blob([data], { type: 'application/pdf' });
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'pdf-tool.pdf'; // Set your desired filename
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
   return (
-    <Link href={href}>
-      <Card className="h-[200px] w-[250px]">
-        <CardHeader className="flex flex-row gap-2">
-          <CgMergeVertical className="w-10 h-10 text-orange-500" />
-          <h2 className="font-semibold text-xl">{title}</h2>
-        </CardHeader>
-        <CardContent>
-          <p>{description}</p>
-        </CardContent>
-      </Card>
-    </Link>
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Card
+          className={cn(
+            'min-h-56 md:min-h-64 w-64 md:w-72 lg:w-80 bg-black hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer',
+            colors[color].bg
+          )}
+        >
+          <CardHeader className="flex flex-row gap-2">
+            <Icon
+              className={cn(
+                'w-10 h-10 text-orange-500',
+                colors[color as keyof typeof colors].text
+              )}
+            />
+            <h2 className="font-semibold text-xl">{title}</h2>
+          </CardHeader>
+          <CardContent>
+            <p>{description}</p>
+          </CardContent>
+        </Card>
+      </DrawerTrigger>
+      <DrawerContent className="w-full min-h-[500px] px-8 py-8 md:px-16">
+        <div className="w-full flex flex-col justify-center items-center gap-4">
+          <DrawerHeader className="flex flex-col justify-center items-center">
+            <DrawerTitle className="text-center text-2xl">{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+            <Icon
+              className={cn(
+                'w-14 h-14 text-orange-500 mt-3',
+                colors[color as keyof typeof colors].text
+              )}
+            />
+          </DrawerHeader>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <div className="border border-dashed p-4 md:p-8">
+              {isDragActive ? (
+                <p>Drop the files here ...</p>
+              ) : (
+                <p>Drag or drop some files here, or click to select files</p>
+              )}
+              {loading && (
+                <div className="flex justify-center items-center mt-4">
+                  <IoReload className="animate-spin w-8 h-8" />
+                </div>
+              )}
+            </div>
+          </div>
+          <div>
+            <p className="font-semibold text-lg text-center">Uploaded</p>
+            {acceptedFiles[0] && (
+              <aside className="flex flex-col justify-center items-center gap-2 mt-2">
+                <FaRegFilePdf className="w-8 h-8" />
+                {acceptedFiles.map((file) => (
+                  <div
+                    key={file.name}
+                    className="w-64 md:w-[32rem] h-auto break-words text-center"
+                  >
+                    {file.name}
+                  </div>
+                ))}
+              </aside>
+            )}{' '}
+          </div>
+
+          <Button className="mt-4" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 }
